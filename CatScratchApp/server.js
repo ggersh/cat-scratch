b5f87e35692e2f0383742dd4098fc7524efd4505
Nic 7aller <n.7aller@gmail.com>
2017-01-21 15:45:57 -0500

    Adding pug

--- CatScratchApp/server.js
+++ CatScratchApp/server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var config = require('./js/app/config');
+var config = require('./js/config');
 var mongoose = require('mongoose');
+const pug = require('pug');
 var app = express();
 var Schema = mongoose.Schema;
 var tweetSchema = new Schema({
@@ -12,15 +13,20 @@ var tweetSchema = new Schema({
     originalTweet: String,
     newTweet: String
 })
+var example = mongoose.model("example", tweetSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.set('view engine', 'pug');
 
-app.get('*', function(req, res) {
-    // get <datalist>
-    // render template
-    res.sendFile(__dirname + 'index.html');
+app.get('/', function(req, res) {
+    example.find({}, 'username', function(err, data) {
+        console.log(data);
+    });
+    res.render('index', {
+        tweets: data
+    });
 });
 
 mongoose.connect(config.dbURL);
