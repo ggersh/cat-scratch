142e2002fb95266bb950210d4fefde6c1837a4e4
Kirstin Boni <boni.14@osu.edu>
2017-01-21 02:59:19 -0500

    Updated streaming.py

--- Streaming.py
+++ Streaming.py
@@ -1,41 +1,45 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 #from Tokenizer import *
 import time
-from checkForHarassment import multipleReplace
+import re
 
-ckey =  'lDxRlm3XWcEBIO7D8q8idMebz'
+
+ckey = 'lDxRlm3XWcEBIO7D8q8idMebz'
 csecret = 'Cy6M1nKIq2aJyyuQRuscB0B8qDPPYpmxTHdwgOJts6L62YaK8e'
-atoken =  '4748567247-oYYHCKqGcsIG9KItdNKtLNzqAEOXXcsNVeLFswI'
+atoken = '4748567247-oYYHCKqGcsIG9KItdNKtLNzqAEOXXcsNVeLFswI'
 asecret = 'yFV5J8TV61kRFiip5teaUFvkwdDlMClWs7kXCqiTmFeOi'
 
 class listener(StreamListener):
-
     def __init__(self):
         self.tweets = []
+        self.dict = {"cavs":u'\ud83d'}
+
+    def multiple_replace(self,text):
+        # Create a regular expression  from the dictionary keys
+        regex = re.compile("(%s)" % "|".join(map(re.escape, self.dict.keys())))
+
+        # For each match, look-up corresponding value in dictionary
+        return regex.sub(lambda mo: self.dict[mo.string[mo.start():mo.end()]], text)
+
 
     def on_data(self,data):
+        tweet = data.split(',"text":"')[1].split('","source')[0]
+
+        new_tweet = self.multiple_replace(tweet)
+        self.tweets.append(data)
+        print(new_tweet)
 
 
-
-            tweet = data.split(',"text":"')[1].split('","source')[0]
-            #print('\n')
-            #print('\n')
-            #print('_____________')
-            self.tweets.append(data)
-            print(tweet)
-            newTweet = multipleReplace(tweet)
-            print(newTweet)
-
-
-
-            #
-            # saveFile = open('newTwitDB.csv','a')
-            # saveFile.write(tweet)
-            # saveFile.write('\n')
-            # saveFile.close()
-            return True
+        saveFile = open('newTwitDB.csv','a')
+        saveFile.write(new_tweet)
+        saveFile.write('\n')
+        saveFile.close()
+        return True
 
 
 
@@ -46,4 +50,4 @@ class listener(StreamListener):
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track = ["ugly"])
+twitterStream.filter(track = ["cavs"])
